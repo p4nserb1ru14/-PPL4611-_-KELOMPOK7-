7d0619473ceff54c8668d865d5b2d3550322c52b
p4nserb1ru14 <luthfi.cool8369@gmail.com>
2021-07-08 11:17:43 +0700

    test

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","a11");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
+
+?>
